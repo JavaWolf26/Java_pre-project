ca46c11f1c191f9134329203414f461545d515cd
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-11-29 00:44:15 +0700

    +28

--- src/main/resources/init-data.sql
+++ src/main/resources/init-data.sql
@@ -1,11 +1,5 @@
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
-INSERT INTO users (firstName, lastName, age, email, enabled, password)
-VALUES ('Admin', 'Adminov', 10, 'admin@email.ru', true,'$2y$12$ewvWbdBj/34U5MC7wHFKZOeBMyc9xRPBMkdhqq/YzA7gT0z21J6oa');
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
 
-INSERT INTO roles (name) VALUES ('ROLE_USER');
-INSERT INTO users (firstName, lastName, age, email, enabled, password)
-VALUES ('User', 'Userov', 11, 'user@email.ru', true, '$2y$12$xr1XtdcnDisJRd1XwJNUTOWXPijwJiVidRS750YfQUuay9zjCEEJy');
-INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO users (age, email, enabled, firstName, lastName,  password) VALUES (10, 'admin@email.ru', true, 'Admin', 'Adminov', '$2y$12$ewvWbdBj/34U5MC7wHFKZOeBMyc9xRPBMkdhqq/YzA7gT0z21J6oa'), (11, 'user@email.ru', true, 'User', 'Userov', '$2y$12$xr1XtdcnDisJRd1XwJNUTOWXPijwJiVidRS750YfQUuay9zjCEEJy');
 
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 2);
+INSERT INTO users_roles (user_id, role_id) VALUES (2, 2), (1, 1), (1, 2);
