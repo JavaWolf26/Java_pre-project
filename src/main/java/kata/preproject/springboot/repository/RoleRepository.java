addbc5a07eeb2f058dc53fdca9db4786d74e8455
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-12-02 01:15:13 +0700

    01

--- src/main/java/kata/preproject/springboot/repository/RoleRepository.java
+++ src/main/java/kata/preproject/springboot/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package kata.preproject.springboot.repository;
 
 import kata.preproject.springboot.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findRoleByName(String name);
 }
--- src/main/java/kata/preproject/springboot/repository/UserRepository.java
+++ src/main/java/kata/preproject/springboot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package kata.preproject.springboot.repository;
 
 import kata.preproject.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
 }
