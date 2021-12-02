5ed077dc5b34b689409c90be089ba9f756ec4f5f
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-12-02 13:45:45 +0700

    01

--- src/main/java/kata/preproject/springboot/controller/AdminController.java
+++ src/main/java/kata/preproject/springboot/controller/AdminController.java
@@ -82,7 +82,7 @@ public class AdminController {
 //        return "redirect:/users";
 //    }
 
-    @DeleteMapping("/users/{id}")
+    @PostMapping("/users/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteUser(id);
         return "redirect:/users";
